2c4fa27ad0290dc8cc342f18c097a1588dd80271
Danilo Herrera <daniloherrera1702@gmail.com>
2022-01-24 20:01:19 -0500

    I little change again for remove two lines of code

--- conversor.py
+++ conversor.py
@@ -1,23 +1,24 @@
-def convertir_pesos():
-    pesos = float(input("Pon los pesos que tengas: "))
+def convertir_pesos(pesos):
+    # pesos = float(input("Pon los pesos que tengas: "))
     valor_dolar = 3974
     dolares = str(round(pesos/valor_dolar, 2))
     print(f"Tienes $ {dolares} dolares")
 
-def convertir_dolares():
-    dolares = float(input("Pon los dolares que tines: "))
+def convertir_dolares(dolares):
+    # dolares = float(input("Pon los dolares que tienes: "))
     valor_pesos = 3974
-    pesos = str(round(valor_pesos * dolares))
+    pesos = str(round(valor_pesos * dolares, 2))
     print(f"Tienes $ {pesos} pesos")
 
 
 def run():
   option = int(input(" 1: Para cambiar pesos a dolares 2: Para cambiar dolares a pesos -> "))
+  money = float(input("Cuanto dinero quieres convertir: "))
   
   if option == 1:
-      convertir_pesos()
+      convertir_pesos(money)
   elif option == 2:
-      convertir_dolares()
+      convertir_dolares(money)
   else:
       print("Coloca uno de los números indicados")
 
