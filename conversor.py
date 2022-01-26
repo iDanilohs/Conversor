f9c44af4c1712b877e4856552dab9f0f895185bd
Danilo Herrera <daniloherrera1702@gmail.com>
2022-01-25 21:47:55 -0500

    This version is the better, because have less code and more functions

--- conversor.py
+++ conversor.py
@@ -1,24 +1,18 @@
-def convertir_pesos(pesos, option2):
-    if option2 == 1:
-        valor_dolar = 3964.97
-    elif option2 == 2:
-        valor_dolar = 104.62
-    elif option2 == 3:
-        valor_dolar = 20.62
+def peso_o_dolar(option, mensaje, money, valor_dolar):
+    if option == 1:
+        convertir_pesos(money, valor_dolar)
+    elif option == 2:
+        convertir_dolares(mensaje, money, valor_dolar)
+    else:
+        print("Coloca uno de los números indicados")
 
+def convertir_pesos(pesos, valor_dolar):
     dolares = str(round(pesos/valor_dolar, 2))
     print(f"Tienes $ {dolares} dolares")
 
-def convertir_dolares(dolares, option2):
-    if option2 == 1:
-        valor_pesos = 3964.97
-    elif option2 == 2:
-        valor_pesos = 104.62
-    elif option2 == 3:
-        valor_pesos = 20.62
-
-    pesos = str(round(valor_pesos * dolares, 2))
-    print(f"Tienes $ {pesos} pesos")
+def convertir_dolares(mensaje, dolares, valor_dolar):
+    pesos = str(round(valor_dolar * dolares, 2))
+    print(f"Tienes $ {pesos} {mensaje}")
 
 
 def run():
@@ -27,16 +21,23 @@ def run():
   2 - Pesos argentinos
   3 - Pesos mexicanos
   Elige una opción: """
+
   option = int(input(" 1: Para cambiar pesos a dolares 2: Para cambiar dolares a pesos -> "))
   money = float(input("Cuanto dinero quieres convertir: "))
   option2 = int(input(menu))
-  
-  if option == 1:
-      convertir_pesos(money, option2)
-  elif option == 2:
-      convertir_dolares(money, option2)
-  else:
-      print("Coloca uno de los números indicados")
+
+  if option2 == 1:
+        valor_dolar = 3964.97
+        mensaje = "Pesos colombianos"
+        peso_o_dolar(option, mensaje, money, valor_dolar)
+  elif option2 == 2:
+        valor_dolar = 104.62
+        mensaje = "Pesos argentinos"
+        peso_o_dolar(option, mensaje, money, valor_dolar)
+  elif option2 == 3:
+        valor_dolar = 20.62
+        mensaje = "Pesos mexicanos"
+        peso_o_dolar(option, mensaje, money, valor_dolar)
 
 if __name__ == '__main__':
     run()
